d120a0cf286674ce8379e060609ec820cf74a653
rsgc-leder-b <bleder@rsgc.on.ca>
2017-02-23 14:28:23 -0500

    Removed unnecessary lines

--- Music Game/Music Game/SongList.swift
+++ Music Game/Music Game/SongList.swift
@@ -17,9 +17,6 @@ class SongList: SKScene {
     
     override func didMove(to view: SKView) {
         backgroundColor = SKColor.blue
-        let background = SKSpriteNode(imageNamed: "splashBackground")
-        background.size = self.frame.size
-        background.position = CGPoint(x: size.width/2, y: size.height/2)
         back.position = CGPoint(x: self.size.width/16, y: self.size.height-self.size.width/32)
         back.size = CGSize(width: self.size.width/8, height: self.size.width/16)
         back.isUserInteractionEnabled = true
